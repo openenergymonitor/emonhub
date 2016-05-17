25537aa43f8c99291549284343a6a34f3201f26d
Trystan Lea <trystan.lea@googlemail.com>
2016-05-17 16:27:56 +0100

    specify path for node additions

--- conf/nodes/emonpi_auto_add_nodes.sh
+++ conf/nodes/emonpi_auto_add_nodes.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 echo "EUID: $EUID"
 emonhub_location=/home/pi/data/emonhub.conf
+path=/home/pi/emonhub/conf/nodes
 
 max=31
 for var in `seq 2 $max`
 do
   if ! grep "\[$var\]" $emonhub_location; then
-    if [ -f $var ]; then
-      cat $var >> $emonhub_location
+    if [ -f $path/$var ]; then
+      cat $path/$var >> $emonhub_location
       echo "">>$emonhub_location
       echo "Added node $var to emonhub.conf"
     fi
