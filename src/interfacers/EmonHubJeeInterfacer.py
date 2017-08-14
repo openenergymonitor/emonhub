56671451fcb2fd6a3a37596b1061ab6f6ae0a701
Stuart Pittaway <stuart@binarycreations.co.uk>
2017-08-14 20:40:03 +0100

    Code tidy up for emonNOTIFY

--- src/interfacers/EmonHubJeeInterfacer.py
+++ src/interfacers/EmonHubJeeInterfacer.py
@@ -63,22 +63,23 @@ class EmonHubJeeInterfacer(ehi.EmonHubSerialInterfacer):
 
         # Jee specific settings to be picked up as changes not defaults to initialise "Jee" device
         self._jee_settings =  ({'baseid': '15', 'frequency': '433', 'group': '210', 'quiet': 'True', 'calibration': '230V' 
-		, 'feedbroadcastinterval': 0
-		, 'feedreadonlyapikey': ''
-		, 'feedurl':'http://localhost/emoncms/feed/fetch.json'
-		, 'feedlisturl':'http://localhost/emoncms/feed/list.json'
-		, 'feedlist':'0', 'feednodeid':4 })
-        self._jee_prefix = ({'baseid': 'i', 'frequency': '', 'group': 'g', 'quiet': 'q', 'calibration': 'p' 
-		,'feedbroadcastinterval':'' 
-		,'feedreadonlyapikey': '' 
-		,'feedurl':'' 
-		,'feedlisturl':'' 
-		,'feedlist':''
-		,'feednodeid':''})
+            ,'feedbroadcastinterval': 0
+            ,'feedreadonlyapikey': ''
+            ,'feedurl':'http://localhost/emoncms/feed/fetch.json'
+            ,'feedlisturl':'http://localhost/emoncms/feed/list.json'
+            ,'feedlist':'0', 'feednodeid':4 })
 
-	# Set default
-	self._interval_timestamp = 0
-	self._feed_interval_timestamp = 0
+        self._jee_prefix = ({'baseid': 'i', 'frequency': '', 'group': 'g', 'quiet': 'q', 'calibration': 'p' 
+            ,'feedbroadcastinterval':'' 
+            ,'feedreadonlyapikey': '' 
+            ,'feedurl':'' 
+            ,'feedlisturl':'' 
+            ,'feedlist':''
+            ,'feednodeid':''})
+
+        # Set default
+        self._interval_timestamp = 0
+        self._feed_interval_timestamp = 0
 
         # Pre-load Jee settings only if info string available for checks
         if all(i in self.info[1] for i in (" i", " g", " @ ", " MHz")):
@@ -170,13 +171,12 @@ class EmonHubJeeInterfacer(ehi.EmonHubSerialInterfacer):
         **kwargs (dict): settings to be modified. Available settings are
         'baseid', 'frequency', 'group'. Example:
         {'baseid': '15', 'frequency': '4', 'group': '210'}
-	emonNotify settings also passed into this function
-        
+
+        emonNotify settings also passed into this function       
         """
 
         for key, setting in self._jee_settings.iteritems():
-
-	    command = ''
+            command = ''
             # Decide which setting value to use
             if key in kwargs.keys():
                 setting = kwargs[key]
@@ -206,7 +206,7 @@ class EmonHubJeeInterfacer(ehi.EmonHubSerialInterfacer):
                 command = '1p'
             elif key == 'calibration' and setting == '110V':
                 command = '2p'
-	    #Settings for emonNOTIFY dont get sent to JEELIB module
+            #Settings for emonNOTIFY dont get sent to JEELIB module
             elif key == 'feedbroadcastinterval' and str(setting).isdigit() and int(setting)>0:
                 setting = int(setting)
             elif key == 'feednodeid'  and str(setting).isdigit() and int(setting)>0:
@@ -218,29 +218,35 @@ class EmonHubJeeInterfacer(ehi.EmonHubSerialInterfacer):
                 continue
 
             self._settings[key] = setting
-	    #self._log.info("%s config setting '%s': '%s'" % (self.name, key, setting))
-	    if (command!=''):
-		#Send to JEELIB Arduino on COM port
+            #self._log.info("%s config setting '%s': '%s'" % (self.name, key, setting))
+            if (command!=''):
+                #Send to JEELIB Arduino on COM port
                 self._log.info("JEELIB setting %s  %s: %s" % (self.name, key, setting) + " (" + command + ")")
                 self._ser.write(command)
                 # Wait a sec between two settings
                 time.sleep(1)
 
+
         # include kwargs from parent
         super(EmonHubJeeInterfacer, self).set(**kwargs)
+
         self.resetEmonNotifyLabelList()
 
+        if (self._settings['feedbroadcastinterval']>0 and self._settings['feedreadonlyapikey']!=""):
+            self._log.info("emonNOTIFY enabled, update interval %i seconds to node %i" % (self._settings['feedbroadcastinterval'],  self._settings['feednodeid']))
+
+
+
     def resetEmonNotifyLabelList(self):
-	"""Reset the emonNotify feed settings and also re-transmit the labels"""
-	#Clone the list
-	self._exported_feeds=list(self._settings['feedlist'])
+        """Reset the emonNotify feed settings and also re-transmit the labels"""
+        #Clone the list if there is one
+        self._exported_feeds=list(self._settings['feedlist'])
         self._exported_sequence=1
 
     def emoncmsfeedvalues(self,url,api,feedid):
-	"""read multiple feed values from EMONCMS parse the JSON reply into Python array
-	"""
+        """read multiple feed values from EMONCMS parse the JSON reply into Python array"""
        
-	data_url=str(url)+"?apikey=" + str(api) + "&ids=" + str(feedid)
+        data_url=str(url)+"?apikey=" + str(api) + "&ids=" + str(feedid)
         try:
                 sock = urllib.urlopen(data_url)
                 data_str = sock.read()
@@ -249,10 +255,9 @@ class EmonHubJeeInterfacer(ehi.EmonHubSerialInterfacer):
         except Exception, detail:
                 self._log.debug(detail)
 
-    def emoncmsfeednames(self,url,api):
-	"""read all feeds from EMONCMS parse the JSON reply into Python array
-        """
 
+    def emoncmsfeednames(self,url,api):
+        """read all feeds from EMONCMS parse the JSON reply into Python array"""
         data_url=str(url)+"?apikey=" + str(api)
         try:
                 sock = urllib.urlopen(data_url)
@@ -282,114 +287,109 @@ class EmonHubJeeInterfacer(ehi.EmonHubSerialInterfacer):
                 self._log.debug(self.name + " broadcasting time: %02d:%02d" % (now.hour, now.minute))
                 self._ser.write("00,%02d,%02d,00,s" % (now.hour, now.minute))
 
+        feedbroadcastinterval = int(self._settings['feedbroadcastinterval'])
+        if (feedbroadcastinterval>0 and self._settings['feedreadonlyapikey']!=""):
+            if (t - self._feed_interval_timestamp) > feedbroadcastinterval:
+                self._feed_interval_timestamp = t
+                self.broadcast_feed_values()
 
-	feedbroadcastinterval = int(self._settings['feedbroadcastinterval'])
-	if (feedbroadcastinterval>0 and self._settings['feedreadonlyapikey']!=""):
-		if (t - self._feed_interval_timestamp) > feedbroadcastinterval:
-			self._feed_interval_timestamp = t
 
-			self._log.debug(self.name + " transmitting feed values over RFM")
 
-			try:
-				output=""
 
-				#emonNOTIFY screen 0 (0-15 allowed)
-				#Message type = 0 (reading/values)
-				headerbyte=(0 & 0x0F)<<4 | (0 & 0x0F)
+    def broadcast_feed_values(self):
+        self._log.debug(self.name + " transmitting feed values over RFM")
+        try:
+            output=""
+            #emonNOTIFY screen 0 (0-15 allowed)
+            #Message type = 0 (reading/values)
+            headerbyte=(0 & 0x0F)<<4 | (0 & 0x0F)
+            output+="%02d," % (headerbyte)
 
-				output+="%02d," % (headerbyte)
+            #Spit out the current time EPOC as 4 bytes
+            #Note this only returns UTC based date/time values
+            now = datetime.datetime.now()
+            stamp = time.mktime(now.timetuple())
+            for b in bytearray( struct.pack('<L', stamp)  ):
+                output+="%02d," % (b)
 
-				#Spit out the current time EPOC as 4 bytes
-				#Note this only returns UTC based date/time values
-				now = datetime.datetime.now()
-				stamp = time.mktime(now.timetuple())
-				for b in bytearray( struct.pack('<L', stamp)  ):
-					output+="%02d," % (b)
+            #Make CSV for feed ids
+            feed_ids=",".join(map(str, self._settings['feedlist']))
+            feed_values=self.emoncmsfeedvalues(self._settings['feedurl'], self._settings['feedreadonlyapikey'], feed_ids)
+            for value in feed_values:
+                # Remove any floating point from the raw value by multiply by 100
+                v = int(float(value) * 100);
+                # Pack as little-endian signed long - 4 bytes
+                barray = bytearray(struct.pack("<l", v))
+                for b in barray:
+                    output+="%02d," % (b)
 
-				feed_ids=",".join(map(str, self._settings['feedlist']))
-				feed_values=self.emoncmsfeedvalues(self._settings['feedurl'], 
-					self._settings['feedreadonlyapikey'], 
-					feed_ids)
+            #Specify the receiver of our message
+            output+="%02d" % ( self._settings['feednodeid'] )
+            output+="s"
+            self._ser.write(output)
+            #self._log.debug(output)
 
-				for value in feed_values:
-					# Remove any floating point from the raw value by multiply by 100
-					v = int(float(value) * 100);
-					# Pack as little-endian signed long - 4 bytes
-					barray = bytearray(struct.pack("<l", v))
-					for b in barray:
-						output+="%02d," % (b)
+            # Now try and send feed text labels to emonNOTIFY
+            self.emonnotify_labels()
 
-				#Specify the receiver of our message
-				output+="%02d," % ( self._settings['feednodeid'] )
-				output+="s"
-				self._ser.write(output)
-				#self._log.debug(output)
+        except Exception, detail:
+            self._log.error(detail)
 
-				# Now try and send feed text labels to emonNOTIFY
-				self.emonnotify_labels()
 
-		        except Exception, detail:
-	                	self._log.debug(detail)
 
     def emonnotify_labels(self):
-	"""Send feed name labels and units of energy remotely to emonNotify via RFM module
-	"""
-	#We only transmit a single label on each loop to prevent swamping the RFM module
-	#and the radio bandwidth and we only transmit the labels once when emonHUB starts up
-	#and again if the configuration changes
-	if len(self._exported_feeds) > 0:
-		feed_list=self.emoncmsfeednames(self._settings['feedlisturl'], self._settings['feedreadonlyapikey'])
+        """Send feed name labels and units of energy remotely to emonNotify via RFM module"""
+        #We only transmit a single label on each loop to prevent swamping the RFM module
+        #and the radio bandwidth and we only transmit the labels once when emonHUB starts up
+        #and again if the configuration changes
+        if len(self._exported_feeds) > 0:
+            feed_list=self.emoncmsfeednames(self._settings['feedlisturl'], self._settings['feedreadonlyapikey'])
+            first_in_list = self._exported_feeds[0]
 
-		first_in_list = self._exported_feeds[0]
+            for feed in feed_list:
+                if str(feed["id"]) in first_in_list:
+                    output=""
+                    # Message type = 1 feed label
+                    headerbyte=(0 & 0x0F)<<4 | (1 & 0x0F)
+                    output+="%02d," % (headerbyte)
+                    output+="%02d," % (self._exported_sequence)
+                    #Label (max 14 chars)
+                    label=feed["name"][:14]
 
-                for feed in feed_list:
-                	if str(feed["id"]) in first_in_list:
-                                output=""
-                                # Message type = 1 feed label
-                                headerbyte=(0 & 0x0F)<<4 | (1 & 0x0F)
-                                output+="%02d," % (headerbyte)
-                                output+="%02d," % (self._exported_sequence)
-                                #Label (max 14 chars)
-                                label=feed["name"][:14]
+                    barray = bytearray(label.encode())
+                    for b in barray:
+                        output+="%02d," % (b)
+                    # Trailing zero byte to terminate string
+                    output+="00,"
+                    units=self.feed_name_to_energy_unit(feed["name"])[:5]
+                    barray = bytearray(units.encode())
+                    for b in barray:
+                        output+="%02d," % (b)
 
-                                barray = bytearray(label.encode())
-                                for b in barray:
-                                	output+="%02d," % (b)
-
-                                # Trailing zero byte to terminate string
-                                output+="00,"
-
-				units=self.feed_name_to_energy_unit(feed["name"])[:5]
-				barray = bytearray(units.encode())
-                                for b in barray:
-                                        output+="%02d," % (b)
-
-				output+="00,"
-
-                                output+="04s"
-				# Wait 2 seconds before transmission to ensure previous feed data message has got there
-				time.sleep(2)
-                                self._ser.write(output)
-
-				self._log.debug("Sending label '%s' and units '%s' sequence %i" %( label, units, self._exported_sequence ))
-
-				self._exported_sequence=self._exported_sequence+1
-                                self._exported_feeds.remove(first_in_list)
-				break
+                    output+="00,"
+                    output+="%02d" % ( self._settings['feednodeid'] )
+                    output+="s"
+                    # Wait 2 seconds before transmission to ensure previous feed data message has got there
+                    time.sleep(2)
+                    self._ser.write(output)
+                    self._log.info("Sending label '%s' and units '%s' sequence %i" %( label, units, self._exported_sequence ))
+                    self._exported_sequence=self._exported_sequence+1
+                    self._exported_feeds.remove(first_in_list)
+                    break
 
 
     def feed_name_to_energy_unit(self,argument):
-	"""Maps commonly used feed names into its unit of energy ideally this would be read from a database contained in emonCMS"""
-	switcher= {
-        	'use': "watts",
-        	'use_kwh': "kW/h",
-		'solar': "watts",
-		'import': "watts",
-		'solar_kwh': "kW/h",
-		'export': "watts",
-		'bmwi3-fuelPercent':'%',
-		'bmwi3-mileage':'miles',
-		'bmwi3-beRemainingRangeElectricMile':'miles',
-		'bmwi3-chargingLevelHv':'% Bat'
-	}
-	return switcher.get(argument, "")
+        """Maps commonly used feed names into its unit of energy ideally this would be read from a database contained in emonCMS"""
+        switcher= {
+            'use': "watts",
+            'use_kwh': "kW/h",
+            'solar': "watts",
+            'import': "watts",
+            'solar_kwh': "kW/h",
+            'export': "watts",
+            'bmwi3-fuelPercent':'%',
+            'bmwi3-mileage':'miles',
+            'bmwi3-beRemainingRangeElectricMile':'miles',
+            'bmwi3-chargingLevelHv':'% Bat'
+        }
+        return switcher.get(argument, "")
