8200b42a92100f62fce2a20f7c6df51e9df0cf92
Trystan Lea <trystan.lea@gmail.com>
2018-03-01 15:05:16 +0000

    update version

--- src/emonhub.py
+++ src/emonhub.py
@@ -46,7 +46,7 @@ Controlled by the user via EmonHubSetup
 
 class EmonHub(object):
 
-    __version__ = "emonHub emon-pi variant v2.1.0"
+    __version__ = "emonHub emon-pi variant v2.1.1"
 
     def __init__(self, setup):
         """Setup an OpenEnergyMonitor emonHub.
