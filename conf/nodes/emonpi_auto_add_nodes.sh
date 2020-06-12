14c774c699c5a2075b36be2490b735ea71e0381b
TrystanLea <trystan.lea@gmail.com>
2020-06-12 12:00:47 +0100

    improved emonpi_auto_add_nodes script implementation thanks to @borpin

--- conf/nodes/emonpi_auto_add_nodes.sh
+++ conf/nodes/emonpi_auto_add_nodes.sh
@@ -17,13 +17,13 @@ fi
 max=31
 for var in `seq 2 $max`
 do
-  if ! grep "\[$var\]" $emonhub_location; then
+  if grep "^\[\[$var\]\]" $emonhub_location; then
+    echo "Node $var already present"
+  else
     if [ -f $path/$var ]; then
       echo "">>$emonhub_location
       cat $path/$var >> $emonhub_location
       echo "Added node $var to emonhub.conf"
     fi
-  else
-    echo "Node $var already present"
   fi
 done
