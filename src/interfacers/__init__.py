a74e8717698176ee1351a435fab1edfbc23a5bad
Trystan Lea <trystan.lea@gmail.com>
2020-09-22 14:05:04 +0100

    include in init

--- src/interfacers/__init__.py
+++ src/interfacers/__init__.py
@@ -16,6 +16,7 @@ __all__ = [
     "EmonModbusTcpInterfacer",
     "EmonHubTeslaPowerWallInterfacer",
     "EmonHubSDS011Interfacer",
+    "EmonHubModbusRenogyInterfacer",
     "EmonHubTemplateInterfacer"
     #"EmonFroniusModbusTcpInterfacer"
 ]
