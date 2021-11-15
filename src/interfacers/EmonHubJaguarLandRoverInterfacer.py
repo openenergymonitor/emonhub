3ef7efe0e25424db86dc98d9c88d9c10e16835ea
Dan Conlon <dan@dansplace.co.uk>
2021-11-15 19:36:58 +0000

    New interfacer to fetch from JLR API

---
+++ src/interfacers/EmonHubJaguarLandRoverInterfacer.py
@@ -0,0 +1,145 @@
+#!/usr/bin/python3
+# EmonHubJaguarLandRoverInterfacer released for use by OpenEnergyMonitor project
+# GNU GENERAL PUBLIC LICENSE -  Version 2, June 1991
+# See LICENCE and README file for details
+
+# Inspiration taken from Stuart Pittaway's EmonHubBMWInterfacer
+
+__author__ = 'Dan Conlon'
+
+import jlrpy
+import sys
+import time
+import traceback
+import Cargo
+from emonhub_interfacer import EmonHubInterfacer
+
+"""class EmonHubJaguarLandRoverInterfacer
+
+Monitors car metrics via Jaguar Land Rover API
+
+"""
+
+class EmonHubJaguarLandRoverInterfacer(EmonHubInterfacer):
+
+    def __init__(self, name, jlrusername='', jlrpassword='', timeinverval=600, duringchargetimeinterval=60, nodeid=28):
+        """Initialize interfacer"""
+
+        # Initialization
+        super().__init__(name)
+
+        self._NodeId = int(nodeid)
+        self._Username = jlrusername
+        self._Password = jlrpassword
+
+        self._time_inverval = int(timeinverval)
+        self._time_inverval_during_charge = int(duringchargetimeinterval)
+        self._reset_duration_timer()
+
+        self._NodeName = name
+        self._first_time_loop = True
+        self._chargingStatus = "UNKNOWN"
+
+# TODO: funnel log lines to emonlogging
+        self._jlrConnection = jlrpy.Connection(self._Username, self._Password)
+
+    def close(self):
+        """Close"""
+        return
+
+    def _reset_duration_timer(self):
+        """Reset timer to current date/time"""
+        self._last_time_reading = time.time()
+
+    def _is_it_time(self):
+        """Checks to see if the duration has expired
+
+        Return true or false
+
+        """
+        duration_of_delay = time.time() - self._last_time_reading
+
+        # CHARGINGACTIVE/NOCHARGING
+        if self._chargingStatus == "CHARGING":
+            # Charging in progress
+            waittime = self._time_inverval_during_charge
+        else:
+            # Not charging
+            waittime = self._time_inverval
+
+        return int(duration_of_delay) > waittime
+
+    # Override base _process_rx code from emonhub_interfacer
+    def _process_rx(self, rxc):
+        if not rxc:
+            return False
+
+        return rxc
+
+    # Override base read code from emonhub_interfacer
+    def read(self):
+        """Read data from JLR API"""
+
+        #Wait until we are ready to read from inverter
+        if not self._is_it_time() and not self._first_time_loop:
+            return
+
+        self._reset_duration_timer()
+
+        self._first_time_loop = False
+
+        try:
+            # Select the first vehicle in the account
+            vehicle = self._jlrConnection.vehicles[0]
+            self._log.debug("Fetching status of VIN %s", vehicle.vin)
+
+            statusResponse = vehicle.get_status()
+
+            coreStatusList = statusResponse['vehicleStatus']['coreStatus']
+            evStatusList = statusResponse['vehicleStatus']['evStatus']
+            statusList = coreStatusList + evStatusList
+            statusDict = {d['key']: d['value'] for d in statusList}
+
+            names = []
+            values = []
+
+            for key in ['ODOMETER_MILES','EV_STATE_OF_CHARGE','EV_RANGE_ON_BATTERY_MILES','EV_RANGE_ON_BATTERY_KM','EV_CHARGING_RATE_SOC_PER_HOUR']:
+                names.append(key)
+                if key in statusDict:
+                    self._log.debug("%s = %s", key, statusDict[key])
+                    values.append(float(statusDict[key]))
+                else:
+                    values.append(-1)
+
+            # OpenEVSE expects charge remaining time in seconds
+            names.append('EV_SECONDS_TO_FULLY_CHARGED')
+            if 'EV_MINUTES_TO_FULLY_CHARGED' in statusDict:
+                self._log.debug("%s = %s", 'EV_MINUTES_TO_FULLY_CHARGED', statusDict['EV_MINUTES_TO_FULLY_CHARGED'])
+                values.append(float(statusDict['EV_MINUTES_TO_FULLY_CHARGED']) * 60)
+            else:
+                values.append(-1)
+
+            # Store if the car is charging or not
+            if 'EV_CHARGING_STATUS' in statusDict:
+                self._log.debug("%s = %s", 'EV_CHARGING_STATUS', statusDict['EV_CHARGING_STATUS'])
+                self._chargingStatus = statusDict['EV_CHARGING_STATUS']
+            names.append("EV_CHARGING_STATUS")
+            if self._chargingStatus == "CHARGING":
+                values.append(1)
+            else:
+                values.append(0)
+
+            c = Cargo.new_cargo()
+            c.rawdata = None
+            c.realdata = values
+            c.names = names
+
+            c.nodeid = self._NodeId
+            c.nodename = self._NodeName
+
+            return c
+
+        except Exception as err2:
+            exc_type, exc_value, exc_traceback = sys.exc_info()
+            self._log.error(err2)
+            self._log.error(repr(traceback.format_exception(exc_type, exc_value, exc_traceback)))
--- src/interfacers/__init__.py
+++ src/interfacers/__init__.py
@@ -13,6 +13,7 @@ __all__ = [
     "EmonHubSMASolarInterfacer",
     "EmonHubGraphiteInterfacer",
     "EmonHubBMWInterfacer",
+    "EmonHubJaguarLandRoverInterfacer",
     "EmonModbusTcpInterfacer",
     "EmonHubTeslaPowerWallInterfacer",
     "EmonHubSDS011Interfacer",
